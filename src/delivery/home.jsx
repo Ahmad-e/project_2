847a0c97092486c3d8626fe1512c1c242dfe60ad
Your Name <you@example.com>
2024-04-22 11:23:50 +0300

    Initialize project using Create React App

---
+++ src/delivery/home.jsx
@@ -0,0 +1,8 @@
+const DelivaryHome=()=>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default DelivaryHome
---
+++ src/employee/home.jsx
@@ -0,0 +1,8 @@
+const EmployeeHome=()=>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default EmployeeHome
