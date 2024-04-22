0d9e00090b4e0ad9f5df8e2211f8d7a3ec657ca4
Your Name <you@example.com>
2024-04-22 13:33:44 +0300

    https://github.com/Ahmad-e/project_2.git

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-//import Button from '@mui/material/Button';
+import './admin/style.css';
+import './delivery/style.css';
+import './employee/style.css';
+import './component/style.css';
+import './user/style.css';
+import './App.css';
+
 
 function App() {
   return (
